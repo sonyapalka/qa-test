175567ab3a1879bdcc0ff256fa1d54bc4a975aaa
sonyapalka <sonya.palka@gmail.com>
2023-08-06 14:14:24 +0200

    Add Mochawesome reporter

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'E2E Tests Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
